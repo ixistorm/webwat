efc034651b9530656c2ecda5351111d56d6cf2a5
Narit Suttipong <totly000@hotmail.com>
2019-07-21 16:43:49 +0700

    J

--- webwat.sql
+++ webwat.sql
@@ -1,98 +1,238 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.6-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             9.5.0.5196
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 21, 2019 at 11:31 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `webwat`
+--
 
--- Dumping database structure for webwat
-DROP DATABASE IF EXISTS `webwat`;
-CREATE DATABASE IF NOT EXISTS `webwat` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `webwat`;
+-- --------------------------------------------------------
 
--- Dumping structure for table webwat.categories
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+--
+-- Table structure for table `banners`
+--
 
--- Dumping data for table webwat.categories: ~3 rows (approximately)
-DELETE FROM `categories`;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+CREATE TABLE `banners` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `link` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `banners`
+--
+
+INSERT INTO `banners` (`id`, `user_id`, `title`, `link`, `path`, `status`, `created_at`, `updated_at`) VALUES
+(2, 1, 'หน้า1', '', '/uploads/banner/f6f2e5d4-7976-44cf-bae7-b7c94696c1c3.png', 1, '2019-07-21 08:18:07', '0000-00-00 00:00:00'),
+(3, 1, 'หน้า2', '', '/uploads/banner/7aa7df13-9c13-4179-9d8a-741e6d3477c1.png', 1, '2019-07-21 08:18:48', '0000-00-00 00:00:00'),
+(4, 1, 'หน้า3', '', '/uploads/banner/1d9a409d-9b72-4f21-aadb-47f48172a939.png', 1, '2019-07-21 08:20:18', '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `categories`
+--
+
 INSERT INTO `categories` (`id`, `name`) VALUES
-	(1, 'ประชาสัมพันธ์'),
-	(2, 'กิจกรรม'),
-	(7, 'mflv[');
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+(1, 'ประชาสัมพันธ์'),
+(2, 'กิจกรรม');
 
--- Dumping structure for table webwat.news
-DROP TABLE IF EXISTS `news`;
-CREATE TABLE IF NOT EXISTS `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `detail` text NOT NULL,
   `thumbnail` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- Dumping data for table webwat.news: ~0 rows (approximately)
-DELETE FROM `news`;
-/*!40000 ALTER TABLE `news` DISABLE KEYS */;
-/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+--
+-- Dumping data for table `news`
+--
 
--- Dumping structure for table webwat.roles
-DROP TABLE IF EXISTS `roles`;
-CREATE TABLE IF NOT EXISTS `roles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+INSERT INTO `news` (`id`, `category_id`, `user_id`, `title`, `detail`, `thumbnail`, `created_at`, `updated_at`) VALUES
+(2, 1, 1, 'ตบกัน', '<p>พระตบกัน</p>', '/uploads/news/93e95487-379f-4345-8f8b-e19b1b7b5563.png', '2019-07-21 08:37:27', '2019-07-21 07:07:10');
 
--- Dumping data for table webwat.roles: ~2 rows (approximately)
-DELETE FROM `roles`;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `detail` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `roles`
+--
+
 INSERT INTO `roles` (`id`, `name`) VALUES
-	(1, 'Admin'),
-	(2, 'User');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+(1, 'Admin'),
+(2, 'User');
 
--- Dumping structure for table webwat.users
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `avatar` varchar(100) DEFAULT NULL,
   `name` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `role_id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- Dumping data for table webwat.users: ~3 rows (approximately)
-DELETE FROM `users`;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+--
+-- Dumping data for table `users`
+--
+
 INSERT INTO `users` (`id`, `avatar`, `name`, `username`, `password`, `role_id`, `email`, `created_at`, `updated_at`) VALUES
-	(1, '/img/150x150.png', 'John Doe', 'admin', '$2b$10$9JJSkZM74p.O4D0BqWSA.eQ5s0D1X9l4uQwqyFkZ68op/Qgkb6Zbu', 1, 'admin@gmail.com', '2019-07-18 22:17:48', '2019-07-18 11:16:11'),
-	(2, '/uploads/avatar/2fb5f795-9bce-4366-95ee-caf556e6576d.png', 'Jack Elit', 'satawat', '$2b$10$nyponB1ZOuOmV682H5XUh.vLSL3XjWb6Nuf7bdCpmNXv5tquTNONy', 2, 'elit@gmail.com', '2019-07-18 22:17:56', '2019-07-18 21:59:02'),
-	(3, '/uploads/avatar/e058176f-5a2d-4e94-820e-c1afa041d710.png', 'Fuck U', 'u', '$2b$10$rH4Yv0ZpE3U8fza.L97cJ.rvwr.L7l2lut/cx/3vmpN9sypDOvTD6', 1, 'u@abcd.com', '2019-07-18 22:18:07', '2019-07-18 22:12:16');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+(1, '/uploads/avatar/c5c73a17-0db3-412a-96c8-2d2c673828b9.png', 'Pokin Inkongngam', 'admin', '$2b$10$BlUnCbWF4GqBOZLRLVlkRO..cxovN96I/c2ZGW7g6VZK3NJAIur6y', 1, '59143425@g.cmru.ac.th', '2019-07-21 05:47:31', '2019-07-21 05:08:49'),
+(2, '/img/150x150.png', 'บัณฑิต นันทะเทศ', 'admin', '$2b$10$FXs/HV26sA81a3HILr8T0um0wXkiTZYeT6VTKacRaWsiU.DYE2UfO', 1, 'silkyland@gmail.com', '2019-07-21 06:21:48', '2019-07-21 06:21:48');
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banners`
+--
+ALTER TABLE `banners`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `banners`
+--
+ALTER TABLE `banners`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
